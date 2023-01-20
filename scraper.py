1766d33d5cca611a80d972b424af7e9b07de9995
Travis <travishill62102@gmail.com>
2023-01-20 18:10:29 -0500

    Misunderstood assignment, fixing

--- scraper.py
+++ scraper.py
@@ -1,40 +1,55 @@
 import urllib.request
 import re
 import random
+from bs4 import BeautifulSoup
 
 
 def getReferenceUrlsOfPage(domain, url):
     webpage = urllib.request.urlopen(url)
     alreadyScrapedURLs = set()
-    websiteMediaExtensions = ["jpg", "png", "mp3", "mp4", "pdf", "mov"]
     mediaURLs = set()
-    for line in webpage:
-            line = line.decode().strip()
-            if domain in line:
-                regexPattern = "href=\"(.*?)\""
-                referencedUrls = re.findall(regexPattern, line)
-                for scrapedURL in referencedUrls:
-                    # handles potential regex mistake of an empty string being found
-                    if len(scrapedURL) == 0:
-                        continue
-                    # Skips scanning the same website
-                    if scrapedURL == url or scrapedURL == url + "/":
-                        continue
-                    #goes to the page associated with the domain (i.e. /events leads to www.rit.edu/events)
-                    if scrapedURL[0] == "/":
-                        scrapedURL = "https://" + domain + scrapedURL
-                    # since we handled the backslash only cases (/events), we can get rid of any URLs/strings that don't have the domain that snuck past
-                    if domain not in scrapedURL:
-                        continue
-                    # You should record, but do not need to search, files that are likely to be media (.jpg, .png, .mp4, etc.)
-                    # Achieve this by splitting on periods and getting the potential file extension (which should be the last index of the list)
-                    mediaTest = scrapedURL.split(".")
-                    if mediaTest[len(mediaTest)-1] in websiteMediaExtensions:
-                        mediaURLs.add(scrapedURL)
-                        continue 
-                    alreadyScrapedURLs.add(scrapedURL)
+    websiteMediaExtensions = ["jpg", "png", "mp3", "mp4", "pdf", "mov", "css"]
+
+    soupify = BeautifulSoup(webpage, 'html.parser')
+
+    links = soupify.select("link")
+    for link in links:
+        linkType = link.get('rel')
+        linkURL = link.get('href')
+        print(linkURL)
+        print(linkType)
+    print(mediaURLs)
+
+    
+    # for line in webpage:
+    #         line = line.decode().strip()
+    #         # if domain in line:
+    #         regexPattern = "href=\"(.*?)\""
+    #         referencedUrls = re.findall(regexPattern, line)
+    #         for scrapedURL in referencedUrls:
+    #             # handles potential regex mistake of an empty string being found
+    #             if len(scrapedURL) == 0:
+    #                 continue
+    #             # Skips scanning the same website
+    #             if scrapedURL == url or scrapedURL == url + "/":
+    #                 continue
+    #             #goes to the page associated with the domain (i.e. /events leads to www.rit.edu/events)
+    #             if scrapedURL[0] == "/":
+    #                 scrapedURL = "https://" + domain + scrapedURL
+    #             # since we handled the backslash only cases (/events), we can get rid of any URLs/strings that don't have the domain that snuck past
+    #             if domain not in scrapedURL:
+    #                 continue
+    #             # You should record, but do not need to search, files that are likely to be media (.jpg, .png, .mp4, etc.)
+    #             # Achieve this by splitting on periods and getting the potential file extension (which should be the last index of the list)
+    #             mediaTest = scrapedURL.split(".")
+    #             if mediaTest[len(mediaTest)-1] in websiteMediaExtensions:
+    #                 mediaURLs.add(scrapedURL)
+    #                 continue 
+    #             alreadyScrapedURLs.add(scrapedURL)
 
     return alreadyScrapedURLs, mediaURLs
+
+
 
 def main():
     allUrlsScraped = set()
@@ -45,20 +60,22 @@ def main():
     scraperParams = inputParams.split(" ")
     domain, url, depth = scraperParams[0], scraperParams[1], scraperParams[2]
     depth = int(depth)
-    while depth >= 0:
-        pageURLs, pageMedia = getReferenceUrlsOfPage(domain, url)
-        for url in pageURLs:
-            allUrlsScraped.add(url)
-        for media in pageMedia:
-            allMediaScraped.add(media)
-        print(url + " " + str(depth))
-        url = random.sample(allUrlsScraped, 1)[0]
-        depth -= 1
-    # # print(len(allUrlsScraped))
+    pageURLs, pageMedia = getReferenceUrlsOfPage(domain, url)
+    # while depth >= 0:
+    #     pageURLs, pageMedia = getReferenceUrlsOfPage(domain, url)
+    #     for url in pageURLs:
+    #         allUrlsScraped.add(url)
+    #     for media in pageMedia:
+    #         allMediaScraped.add(media)
+    #     # print(url + " " + str(depth))
+    #     # url = random.sample(allUrlsScraped, 1)[0]
+    #     depth -= 1
+    # # # print(len(allUrlsScraped))
     # # print(len(allMediaScraped))
     # print(allUrlsScraped)
     print("Total Amount of URLs Scraped: " + str(len(allUrlsScraped)))
     print("Total Amount of Media Scraped: " + str(len(allMediaScraped)))
+    # print(allMediaScraped)
     
 if __name__ == "__main__":
     main()
