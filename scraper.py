05d30c0da0f4df20fc705dbc960ecbc802852dcd
Travis <travishill62102@gmail.com>
2023-01-20 23:06:58 -0500

    I am now realizing I probably could have done all the repetitive stuff in my main. Oops

--- scraper.py
+++ scraper.py
@@ -1,72 +1,87 @@
 import urllib.request
-import re
-import random
+import urllib.error
 from bs4 import BeautifulSoup
 
 
 def getReferenceUrlsOfPage(domain, url):
-    webpage = urllib.request.urlopen(url)
-    # for line in webpage:
-    #     print(line.decode().strip())
-    htmlURLs = set()
-    javaScriptURLs = set()
-    cssURLs = set()
-    urlList = []
+    try:
+        webpage = urllib.request.urlopen(url)
+        # for line in webpage:
+        #     print(line.decode().strip())
+        htmlURLs = set()
+        javaScriptURLs = set()
+        cssURLs = set()
+        urlList = []
 
-    soupify = BeautifulSoup(webpage)
+        soupify = BeautifulSoup(webpage)
 
-    # Handles hyperlinks in HTML
-    hyperlinks = soupify.findAll("a")
-    for hyperlink in hyperlinks:
-        hyperlinkURL = hyperlink.get('href')
-        # A link should have an href tag and not be empty to be included 
-        if hyperlinkURL != None and len(hyperlinkURL) != 0:
-            # Check to see if result is unique to the domain
-            if hyperlinkURL[0] == "/":
-                #add domain to make a full url
-                hyperlinkURL = "https://" + domain + hyperlinkURL
-                #Scrape this website as well
-            htmlURLs.add(hyperlinkURL)
+        # Handles hyperlinks in HTML
+        hyperlinks = soupify.findAll("a")
+        for hyperlink in hyperlinks:
+            hyperlinkURL = hyperlink.get('href')
+            # A link should have an href tag and not be empty to be included 
+            if hyperlinkURL != None and len(hyperlinkURL) != 0:
+                # Check to see if result is unique to the domain
+                if hyperlinkURL[0] == "/" or hyperlinkURL[0] == "#":
+                    #add domain to make a full url
+                    hyperlinkURL = "https://" + domain + hyperlinkURL
+                htmlURLs.add(hyperlinkURL)
 
-    # Handles JavaScript.
-    srcs = soupify.findAll("script")
-    for src in srcs:
-        srcURL = src.get('src')
-        # A script should have an src tag and not be empty to be included.
-        if srcURL != None and len(srcURL) != 0:
-            # We want the urls to be on the same domain, so we check for the keyword.
-            splitDomain = domain.split(".")
-            #In theory it should always be in the 1st spot since www (DOT) domainName (DOT) com
-            if splitDomain[1] not in srcURL:
-                print(srcURL)
-                continue
-            javaScriptURLs.add(srcURL)
+        # Handles JavaScript.
+        srcs = soupify.findAll("script")
+        for src in srcs:
+            srcURL = src.get('src')
+            # A script should have an src tag and not be empty to be included.
+            if srcURL != None and len(srcURL) != 0:
+                # We want the urls to be on the same domain, so we check for the keyword.
+                splitDomain = domain.split(".")
+                #In theory it should always be in the 1st spot since www (DOT) domainName (DOT) com
+                if splitDomain[1] in srcURL:
+                    if srcURL[0] == "/" or srcURL[0] == "#":
+                        #add domain to make a full url
+                        srcURL = "https://" + domain + srcURL
+                    # don't add to javaScript set if its in HTML set already. No need for duplicates.
+                    if srcURL in htmlURLs:
+                        continue
+                    javaScriptURLs.add(srcURL)
 
-    # Handles CSS
-    links = soupify.findAll("link")
-    for link in links:
-        linkURL = link.get("href")
-        # We want the urls to be on the same domain, so we check for the keyword.
-        splitDomain = domain.split(".")
-        #In theory it should always be in the 1st spot since www (DOT) domainName (DOT) com
-        if splitDomain[1] in linkURL:
-            if linkURL in htmlURLs:
-                continue
-            cssURLs.add(linkURL)
+        # Handles CSS
+        links = soupify.findAll("link")
+        for link in links:
+            linkURL = link.get("href")
+            # A link should have an href tag and not be empty to be included.
+            if linkURL != None and len(linkURL) != 0:
+                # We want the urls to be on the same domain, so we check for the keyword.
+                splitDomain = domain.split(".")
+                #In theory it should always be in the 1st spot since www (DOT) domainName (DOT) com
+                if splitDomain[1] in linkURL:
+                    if linkURL[0] == "/" or linkURL[0] == "#":
+                        #add domain to make a full url
+                        linkURL = "https://" + domain + linkURL
+                    # don't add to CSS set if its in HTML/javasScript. No need for duplicates.
+                    if linkURL in htmlURLs or linkURL in javaScriptURLs:
+                        continue
+                    cssURLs.add(linkURL)
 
-    # adding all the sets into a list to make return value easier to handle
-    urlList.append(htmlURLs)
-    urlList.append(javaScriptURLs)
-    urlList.append(cssURLs)
+        # adding all the sets into a list to make return value easier to handle
+        urlList.append(htmlURLs)
+        urlList.append(javaScriptURLs)
+        urlList.append(cssURLs)
 
-    return urlList
+        return urlList
+
+    except urllib.error.HTTPError as e:
+        print(url, "gave an http error", e)
+        pass
+
+    
 
 
 
 def main():
     allUrlsScraped = set()
     # "This web scraper will take three pieces of input:  a domain, a URL, and a depth."
-    print("Please enter a domain, a URL including https://, and a depth all separated by spaces. i.e. https://www.rit.edu https://www.rit.edu 2")
+    print("Please enter a domain, a URL including https://, and a depth all separated by spaces. i.e. www.rit.edu https://www.rit.edu 2")
     inputParams = input("Enter here: ")
     scraperParams = inputParams.split(" ")
     domain, url, depth = scraperParams[0], scraperParams[1], scraperParams[2]
