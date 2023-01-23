2ddc0955f4481af149295021e3b7c1e16e2eed8d
Travis <travishill62102@gmail.com>
2023-01-23 15:01:21 -0500

    I don't know how a duplicate value is in a set

--- scraper.py
+++ scraper.py
@@ -33,9 +33,9 @@ def getReferenceUrlsOfPage(domain, url):
                     if hyperlinkURL[0] == "/" or hyperlinkURL[0] == "#":
                         #add domain to make a full url
                         hyperlinkURL = httpsDomain + hyperlinkURL
-                    domainChecker = hyperlinkURL.split(".")
-                    if domainChecker[0] == "https://www":
-                        htmlURLs.add(hyperlinkURL)
+                    # If the start of the spliced URL does not match the domain then it should be ignored.
+                    if hyperlinkURL[:len(httpsDomain)] == httpsDomain:
+                        allUrlsScraped.add(hyperlinkURL.strip())
 
         # Handles JavaScript.
         srcs = soupify.findAll("script")
@@ -51,9 +51,9 @@ def getReferenceUrlsOfPage(domain, url):
                     # don't add to javaScript set if its in HTML set already. No need for duplicates.
                     if srcURL in htmlURLs:
                         continue
-                    domainChecker = srcURL.split(".")
-                    if domainChecker[0] == "https://www":
-                        javaScriptURLs.add(srcURL)
+                    # If the start of the spliced URL does not match the domain then it should be ignored.
+                    if srcURL[:len(httpsDomain)] == httpsDomain:
+                        allUrlsScraped.add(srcURL.strip())
 
         # Handles CSS
         links = soupify.findAll("link")
@@ -69,16 +69,9 @@ def getReferenceUrlsOfPage(domain, url):
                     # don't add to CSS set if its in HTML/javasScript. No need for duplicates.
                     if linkURL in htmlURLs or linkURL in javaScriptURLs:
                         continue
-                    domainChecker = linkURL.split(".")
-                    if domainChecker[0] == "https://www":
-                        cssURLs.add(linkURL)
-
-        # adding all the sets into a list to make return value easier to handle
-        urlList.append(htmlURLs)
-        urlList.append(javaScriptURLs)
-        urlList.append(cssURLs)
-
-        return urlList
+                    # If the start of the spliced URL does not match the domain then it should be ignored.
+                    if linkURL[:len(httpsDomain)] == httpsDomain:
+                        allUrlsScraped.add(linkURL.strip())
 
     except urllib.error.HTTPError as e:
         print(url, "gave an http error", e)
@@ -90,17 +83,7 @@ def execute_queue(domain, url_queue):
     while q_full:
         try:
             url = url_queue.get(False)
-            urlLists = getReferenceUrlsOfPage(domain, url)
-            # If it = None this indicates some HTTP error most likely happened. The thread/site crawl will not be ran.
-            # Should NOT affect the program from running.
-            # Right now there is a bug with creating urls with the first character starting with /
-            # Lots of webpages scraped are also old and don't exist anymore. NOT BUG RELATED
-            if urlLists == None:
-                pass
-            else:
-                for urlList in urlLists:
-                    for url in urlList:
-                        allUrlsScraped.add(url)
+            getReferenceUrlsOfPage(domain, url)
         except queue.Empty:
             q_full = False
 
@@ -121,10 +104,7 @@ def main():
     while castDepth >= 0:
         # if its the first iteration we want to gather all the future urls from our initial input url
         if castDepth == int(depth):
-            urlLists = getReferenceUrlsOfPage(domain, url)
-            for urlList in urlLists:
-                for url in urlList:
-                    allUrlsScraped.add(url)
+            getReferenceUrlsOfPage(domain, url)
             print("Total Amount of URLs Scraped at depth " + str(counter) + " is " + str(len(allUrlsScraped)))
         else:
             # A queue handles all the locking and stuff in threading, easy to use
@@ -132,11 +112,6 @@ def main():
             # Then we use the urls we scraped from the first url and then scrape those.
             iterableScapedUrls = list(allUrlsScraped)
             for url in iterableScapedUrls:
-                # If the start of the spliced URL does not match the domain then it should be ignored.
-                domainWithHttps = "https://" + domain
-                splicedUrl = url[0:len(domainWithHttps)]
-                if splicedUrl != domainWithHttps:
-                    continue
                 q.put(url)
                 t = threading.Thread(target=execute_queue, args=(domain,q,))
                 t.start()
